854620d25d9a806ad2889cd6551b3a022ddb0060
Paul <paularnacfr@gmail.com>
2026-03-04 20:13:31 -0500

    feat(sdk): add React hooks barrel export

---
+++ src/react/index.ts
@@ -0,0 +1,8 @@
+export { useChat } from "./use-chat"
+export type { UseChatOptions, UseChatReturn } from "./use-chat"
+
+export { useCompletion } from "./use-completion"
+export type { UseCompletionOptions, UseCompletionReturn } from "./use-completion"
+
+export { useVectorSearch } from "./use-vector-search"
+export type { UseVectorSearchOptions, UseVectorSearchReturn } from "./use-vector-search"
